94efe689dc185f1faf0d62e0042b7eb2bf8fe9a0
kykauwi <themagnus1208@gmail.com>
2020-10-23 17:19:00 +0200

    Oppgave 1 passerer nå alle tester etter at programkode fra kompendiet ble lagt inn og jeg la inn en liten endring i opprettelsen av noden.

--- src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -82,11 +82,28 @@ public class EksamenSBinTre<T> {
 
     public boolean leggInn(T verdi) {
         // Bruke programkode fra kompendie for aa opprette nye noder
+        Objects.requireNonNull(verdi, "Ulovlig med nullverdier!");
 
-        // Teste og se hvilke endringer som trengs for aa opprette ny node uten feil pekere og feil i testen
+        Node<T> p = rot, q = null;               // p starter i roten
+        int cmp = 0;                             // hjelpevariabel
 
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        while (p != null)       // fortsetter til p er ute av treet
+        {
+            q = p;                                 // q er forelder til p
+            cmp = comp.compare(verdi,p.verdi);     // bruker komparatoren
+            p = cmp < 0 ? p.venstre : p.høyre;     // flytter p
+        }
 
+        // p er nå null, dvs. ute av treet, q er den siste vi passerte
+
+        p = new Node<>(verdi,q);                   // oppretter en ny node
+
+        if (q == null) rot = p;                  // p blir rotnode
+        else if (cmp < 0) q.venstre = p;         // venstre barn til q
+        else q.høyre = p;                        // høyre barn til q
+
+        antall++;                                // én verdi mer i treet
+        return true;                             // vellykket innlegging
     }
 
     public boolean fjern(T verdi) {
